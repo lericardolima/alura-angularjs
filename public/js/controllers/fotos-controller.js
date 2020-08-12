9b4f472d2a8b8ae420a57a0a45e84296a9dfb7a9
lericardolima <lericardolima@gmail.com>
2020-08-12 19:56:57 -0300

    Criando o primeiro module e controller

---
+++ public/js/controllers/fotos-controller.js
@@ -0,0 +1,7 @@
+angular.module('alurapic').controller('FotosController', function($scope) {
+
+    $scope.foto = {
+        titulo: "Leão",
+        url: "https://cdn.leroymerlin.com.br/products/quadro_leao_bronze_100x100cm_90658085_0001_600x600.jpg"
+    }
+});
